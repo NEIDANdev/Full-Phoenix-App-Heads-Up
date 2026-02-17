142e88be5157c7d2aeaeb19b2f7726bb5d269a38
Diego San Vicente <neidan.arq.s@gmail.com>
2026-02-17 17:24:26 -0600

    Add a new custom component for incident endpoint
    In order to learn about how to add HEEx content inside a function
    component, we created a new function component that add render_slot/1
    in order to create more dynamic html elements

--- lib/heads_up_web/components/custom_components.ex
+++ lib/heads_up_web/components/custom_components.ex
@@ -12,7 +12,25 @@ defmodule HeadsUpWeb.CustomComponents do
       @status == :pending && "text-amber-600 border-amber-600",
       @status == :canceled && "text-gray-600 border-gray-600"
     ]}>
-    {@status}
+      {@status}
+    </div>
+    """
+  end
+
+  slot :inner_block, required: true
+  slot :tagline, required: false
+
+  def headline(assigns) do
+    assigns = assign(assigns, :emoji, ~w(🤩 😎 😱) |> Enum.random())
+
+    ~H"""
+    <div class="headline">
+      <h1>
+        {render_slot(@inner_block)}
+      </h1>
+      <div class="tagline">
+        {render_slot(@tagline, @emoji)}
+      </div>
     </div>
     """
   end
